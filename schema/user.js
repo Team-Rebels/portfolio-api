88d69e55ff0c81cc4583c37b0c6f341aa2926ab9
FAMfreda <mbordfreda@gmail.com>
2024-07-10 09:13:54 +0000

    added a user schema

---
+++ schema/user.js
@@ -0,0 +1,18 @@
+import Joi, { required } from "joi";
+
+const userSchema = Joi.object({
+    user:{
+    firstName:Joi.string().required(),
+    lastName:Joi.string().required(),
+    otherName:Joi.string(),
+    email:Joi.string().email().required(),
+    password:Joi.string().min().required(),
+    confirmedPassword:Joi.ref('password')
+    userName: Joi.string(),
+    termsAndConditions: Joi.boolean(),
+
+
+
+
+}
+})
