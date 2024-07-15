661bcab2ba9d746563f0af62d612cca9b67615c6
larem <emmanuelosei831@gmail.com>
2024-07-15 14:12:54 +0000

    install cors

--- routes/achievement_routes.js
+++ routes/achievement_routes.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
-import { getUserAchievements, updateAchievement ,deleteAchievement, addAchievement } from "../controllers/achievement_controller.js";
 import { checkUserSession } from "../middlewares/auth.js";
-
+import { createUserAchievement } from "../controllers/achievement_controller.js";
 
 
 
@@ -10,19 +9,19 @@ const AchievementRouter = Router()
 
 //define routes
 //add a contact route
-AchievementRouter.post('/achievement',checkUserSession, addAchievement)
+AchievementRouter.post('/achievement',checkUserSession,createUserAchievement)
 
-//get route
-AchievementRouter.get('/achievement',checkUserSession, getUserAchievements)
+// //get route
+// AchievementRouter.get('/achievement',checkUserSession, getUserAchievements)
 
-// //get by id
-// AchievementRouter.get('/achievement/:id', oneAchievement)
+// // //get by id
+// // AchievementRouter.get('/achievement/:id', oneAchievement)
 
-//update contact
-AchievementRouter.patch('/achievement/:id', updateAchievement)
+// //update contact
+// AchievementRouter.patch('/achievement/:id', updateAchievement)
 
-//delete contact
-AchievementRouter.delete('/achievement/:id',deleteAchievement)
+// //delete contact
+// AchievementRouter.delete('/achievement/:id',deleteAchievement)
 
 
 export default AchievementRouter;
